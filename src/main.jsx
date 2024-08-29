c68f71e91ce24f439334a2f492e19f61bb20e5e2
João Vitor de Souza <143558833+JoaoVitor2004@users.noreply.github.com>
2024-08-29 15:02:36 -0300

    adicionar novas alterações

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
-import Test from './pages/Test/Test.jsx'
-import Effect from './pages/useEffect/Effect.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
@@ -11,8 +9,6 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/Test' element={<Test/>}></Route>
-        <Route path='/Effect' element={<Effect/>}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
